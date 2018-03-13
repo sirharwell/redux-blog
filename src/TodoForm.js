cdd82829bed293236a94fa9d7c7944fbb81c24a5
Ian Harwell <sirharwell@gmail.com>
2018-03-13 17:28:24 -0600

    color

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -20,14 +20,24 @@ class TodoForm extends React.Component {
     this.setState({ name: e.target.value })
   }
 
+  getRandomColor() {
+    var letters = '789ABCD';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 6)];
+    }
+    return color;
+  }
+
   render() {
     const { name } = this.state
 
     return (
-      <div>
+      <div background-color={this.getRandomColor}>
         <h3>Add A Todo</h3>
         <form onSubmit={this.handleSubmit}>
           <input value={name} onChange={this.handleChange} />
+          <div/>
         </form>
       </div>
     )
